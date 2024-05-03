cf2664868deecb42e5001008b812970777a38b00
dami <devdami0828@gmail.com>
2024-05-03 23:56:11 +0900

    delete module.css

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
-import styles from "./Todos.module.css";
 
 export default function AddTodo({ onAddTodo }) {
   const [text, setText] = useState("");
@@ -15,15 +14,14 @@ export default function AddTodo({ onAddTodo }) {
     setText("");
   };
   return (
-    <form onSubmit={handleSubmit} className={styles.todoForm}>
+    <form onSubmit={handleSubmit}>
       <input
         type="text"
         placeholder="오늘 할일을 등록해주세요"
         value={text}
         onChange={handleChange}
-        className={styles.todoTextInput}
       />
-      <button className={styles.todoCreateButton}>등록</button>
+      <button>등록</button>
     </form>
   );
 }
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,16 +1,13 @@
 import React from "react";
-import styles from "./Todos.module.css";
 
 export default function TodoItem({ todo, onDelete }) {
   const handleDelete = () => {
     onDelete(todo);
   };
   return (
-    <li className={styles.todo}>
+    <li>
       <p>{todo.text}</p>
-      <button className={styles.todoDeleteButton} onClick={handleDelete}>
-        삭제
-      </button>
+      <button onClick={handleDelete}>삭제</button>
     </li>
   );
 }
