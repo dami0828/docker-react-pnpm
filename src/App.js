6e42130b1705a25c655978bae0c13f9ae6bcff3c
dami <devdami0828@gmail.com>
2024-05-03 23:39:41 +0900

    module.css 경로 확인

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import styles from "./App.module.css";
+// import styles from "./App.module.css";
 import TodoList from "./components/TodoList";
 
 function App() {
   return (
-    <div className={styles.container}>
-      <section className={styles.todoContainer}>
-        <h1 className={styles.title}>오늘의 할일</h1>
+    <div>
+      <section>
+        <h1>오늘의 할일</h1>
         <TodoList />
       </section>
     </div>
+    // <div className={styles.container}>
+    //   <section className={styles.todoContainer}>
+    //     <h1 className={styles.title}>오늘의 할일</h1>
+    //     <TodoList />
+    //   </section>
+    // </div>
   );
 }
 
